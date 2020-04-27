e40cc23e1abb673333cd470624e837c9642f0803
Anton Telyshev <anton.telishev@yandex.ru>
2020-04-27 19:35:04 +0300

    Remove goleak from hw06

--- hw06_pipeline_execution/go.mod
+++ hw06_pipeline_execution/go.mod
@@ -2,7 +2,4 @@ module github.com/fixme_my_friend/hw06_pipeline_execution
 
 go 1.14
 
-require (
-	github.com/stretchr/testify v1.5.1
-	go.uber.org/goleak v1.0.0
-)
+require github.com/stretchr/testify v1.5.1
--- hw06_pipeline_execution/pipeline_test.go
+++ hw06_pipeline_execution/pipeline_test.go
@@ -6,7 +6,6 @@ import (
 	"time"
 
 	"github.com/stretchr/testify/require"
-	"go.uber.org/goleak"
 )
 
 const (
@@ -15,8 +14,6 @@ const (
 )
 
 func TestPipeline(t *testing.T) {
-	defer goleak.VerifyNone(t)
-
 	// Stage generator
 	g := func(name string, f func(v I) I) Stage {
 		return func(in In) Out {
