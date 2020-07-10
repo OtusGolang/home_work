e7b644cd7a5beb5e1a4516e8137bc996c5417d8d
Yanis <yanis.urbis@gmail.com>
2020-07-10 20:35:27 +0300

    wait for operation

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,15 +1,25 @@
 package main
 
 import (
-	"bytes"
-	"io/ioutil"
+	"os"
+	"sync"
+	"time"
 )
 
 func main() {
 	// Place your code here
 	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
-	in := &bytes.Buffer{}
-	out := &bytes.Buffer{}
+	//in := &bytes.Buffer{}
+	//out := &bytes.Buffer{}
+	var wg sync.WaitGroup
+	wg.Add(1)
 
-	NewTelnetClient("0.0.0.0:4242", 1, ioutil.NopCloser(in), out)
+	NewTelnetClient("0.0.0.0:4242", 1, os.Stdin, os.Stdout)
+
+	go func(wg *sync.WaitGroup) {
+		defer wg.Done()
+		time.Sleep(time.Second * 5)
+	}(&wg)
+
+	wg.Wait()
 }
