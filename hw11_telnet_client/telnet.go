b3b6819c6e9cd469b5a296f93f95c286eb705af7
Yanis <yanis.urbis@gmail.com>
2020-07-13 20:47:15 +0300

    add ability to send message between client and server

--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io"
+	"log"
 	"net"
 	"time"
 )
@@ -38,6 +40,8 @@ func NopTelnetClient(c net.Conn) TelnetClient {
 	return hello{c}
 }
 
+
+
 func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
 	// Place your code here
 	conn, err := net.Dial("tcp", address)
@@ -45,9 +49,36 @@ func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, ou
 		fmt.Println("No connection!")
 		return nil
 	}
-	for x := range time.Tick(time.Second) {
-		conn.Write([]byte(fmt.Sprintf("Time: %d", x)))
-	}
+	//for x := range time.Tick(time.Second) {
+	//	conn.Write([]byte(fmt.Sprintf("Time: %d", x)))
+	//}
+
+	go func(conn net.Conn, in io.ReadCloser) {
+		scanner := bufio.NewScanner(in)
+		scanner.Split(bufio.ScanLines)
+		for scanner.Scan() {
+			// TODO: put on one line
+			_, err = conn.Write(scanner.Bytes())
+			conn.Write([]byte("\n"))
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+	}(conn, in)
+
+	go func(conn net.Conn, out io.Writer) {
+		scanner := bufio.NewScanner(conn)
+		scanner.Split(bufio.ScanLines)
+		for scanner.Scan() {
+			// TODO: put on one line
+			_, err = out.Write(scanner.Bytes())
+			out.Write([]byte("\n"))
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+	}(conn, out)
+
 	return NopTelnetClient(conn)
 }
 
