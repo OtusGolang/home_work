738ae1f698b317863bbaeffd8185a7cfa7b59260
Yanis <yanis.urbis@gmail.com>
2020-07-20 21:57:11 +0300

    add argument parsing

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,32 +1,59 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"os/signal"
-	"sync"
-	"syscall"
-	"time"
+	"flag"
+	"log"
+	"strconv"
 )
 
+type Args struct {
+	host string
+	port string
+	timeout int
+}
+
+func getArgs() *Args {
+	timeout := flag.String("timeout", "10s", "connection timeout")
+	flag.Parse()
+	otherArgs := flag.Args()
+
+	args := Args{
+		host: otherArgs[0],
+		port: otherArgs[1],
+		timeout: func() int {
+			timeoutStr := *timeout
+			timeoutInt, err := strconv.Atoi(timeoutStr[:len(timeoutStr)-1])
+			if err != nil {
+				log.Fatal("Timeout format is not correct")
+			}
+			return timeoutInt
+		}(),
+	}
+
+	if args.host == "" {
+		log.Fatal("Specify 1st parameter: the host")
+	}
+	if args.port == "" {
+		log.Fatal("Specify 2nd parameter: the port")
+	}
+
+	return &args
+}
+
 func main() {
-	// Place your code here
-	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
-	//in := &bytes.Buffer{}
-	//out := &bytes.Buffer{}
-	var wg sync.WaitGroup
-	wg.Add(1)
-
-	tc := NewTelnetClient("0.0.0.0:4242", 10*time.Second, os.Stdin, os.Stdout)
-	tc.Connect()
-	fmt.Printf("%+v\n", tc)
-	go func(wg *sync.WaitGroup, tc TelnetClient) {
-		defer wg.Done()
-		c := make(chan os.Signal)
-		signal.Notify(c, os.Interrupt, syscall.SIGTERM)
-		<-c
-		tc.Close()
-	}(&wg, tc)
-
-	wg.Wait()
+	//var wg sync.WaitGroup
+	//wg.Add(1)
+	//
+	//tc := NewTelnetClient("0.0.0.0:4242", 10*time.Second, os.Stdin, os.Stdout)
+	//tc.Connect()
+	//fmt.Printf("%+v\n", tc)
+	//go func(wg *sync.WaitGroup, tc TelnetClient) {
+	//	defer wg.Done()
+	//	c := make(chan os.Signal)
+	//	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	//	<-c
+	//	tc.Close()
+	//}(&wg, tc)
+	//
+	//wg.Wait()
 }
