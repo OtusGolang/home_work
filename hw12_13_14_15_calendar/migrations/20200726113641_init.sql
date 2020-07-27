6c011b1d5d713689ce89cb79770bed30d4093f9d
Yanis <yanis.urbis@gmail.com>
2020-07-27 11:41:01 +0300

    working migration

--- hw12_13_14_15_calendar/migrations/20200726113641_init.sql
+++ hw12_13_14_15_calendar/migrations/20200726113641_init.sql
@@ -1,26 +1,15 @@
 -- +goose Up
-CREATE table users (
-    id serial primary key,
-    name varchar(50)
+CREATE TABLE post (
+    id int NOT NULL,
+    title text,
+    body text,
+    PRIMARY KEY(id)
 );
 
--- CREATE table events (
---     id  serial primary key,
---     title varchar(50),
---     start_at timestamp,
---     end_at timestamp,
---     description varchar(1000),
---     user_id serial,
---     notify_at timestamp,
---     constraint fk_user foreign key(user_id) references users(id)
--- );
-
-INSERT INTO users (name)
-VALUES ('Anna'), ('Bob'), ('Carl');
-
--- SQL in this section is executed when the migration is applied.
+INSERT INTO post(id, title, body)
+VALUES (1, '1', '1'),
+        (2, '2', '2'),
+        (3, '3', '3');
 
 -- +goose Down
-drop table events;
-drop table users;
--- SQL in this section is executed when the migration is rolled back.
+DROP TABLE post;
