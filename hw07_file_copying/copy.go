f1a12c2d923e3dd69268fd7e461e94a2d2770bee
Alexander Melentyev <55826637+alexander-melentyev@users.noreply.github.com>
2020-11-06 17:28:45 +0300

    refactor(hw07_file_copying): Removed arg type from copy func (#19)

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -9,7 +9,7 @@ var (
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
 )
 
-func Copy(fromPath string, toPath string, offset, limit int64) error {
+func Copy(fromPath, toPath string, offset, limit int64) error {
 	// Place your code here
 	return nil
 }
