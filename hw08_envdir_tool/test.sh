dc50c1595d06c16aa17f54d093e455a2590ddeca
Антон Телышев <anton.telishev@yandex.ru>
2020-11-18 09:31:49 +0300

    boost bash tests in hw08

--- hw08_envdir_tool/test.sh
+++ hw08_envdir_tool/test.sh
@@ -6,6 +6,8 @@ go build -o go-envdir
 export HELLO="SHOULD_REPLACE"
 export FOO="SHOULD_REPLACE"
 export UNSET="SHOULD_REMOVE"
+export ADDED="from original env"
+export EMPTY="SHOULD_BE_EMPTY"
 
 result=$(./go-envdir "$(pwd)/testdata/env" "/bin/bash" "$(pwd)/testdata/echo.sh" arg1=1 arg2=2)
 expected='HELLO is ("hello")
@@ -13,6 +15,8 @@ BAR is (bar)
 FOO is (   foo
 with new line)
 UNSET is ()
+ADDED is (from original env)
+EMPTY is ()
 arguments are arg1=1 arg2=2'
 
 [ "${result}" = "${expected}" ] || (echo -e "invalid output: ${result}" && exit 1)
--- hw08_envdir_tool/testdata/echo.sh
+++ hw08_envdir_tool/testdata/echo.sh
@@ -4,4 +4,6 @@ echo -e "HELLO is (${HELLO})
 BAR is (${BAR})
 FOO is (${FOO})
 UNSET is (${UNSET})
+ADDED is (${ADDED})
+EMPTY is (${EMPTY})
 arguments are $*"
