2a601ef01908911b9a08d0249fbdafbba519a728
Aleksey Bakin <alexey.bakin@gmail.com>
2020-12-12 13:05:58 +0300

    refactor(hw02): clean tests

--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -1,80 +1,45 @@
 package hw02_unpack_string //nolint:golint,stylecheck
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
-type test struct {
-	input    string
-	expected string
-	err      error
-}
-
 func TestUnpack(t *testing.T) {
-	for _, tst := range [...]test{
-		{
-			input:    "a4bc2d5e",
-			expected: "aaaabccddddde",
-		},
-		{
-			input:    "abccd",
-			expected: "abccd",
-		},
-		{
-			input:    "3abc",
-			expected: "",
-			err:      ErrInvalidString,
-		},
-		{
-			input:    "45",
-			expected: "",
-			err:      ErrInvalidString,
-		},
-		{
-			input:    "aaa10b",
-			expected: "",
-			err:      ErrInvalidString,
-		},
-		{
-			input:    "",
-			expected: "",
-		},
-		{
-			input:    "aaa0b",
-			expected: "aab",
-		},
-	} {
-		result, err := Unpack(tst.input)
-		require.Equal(t, tst.err, err)
-		require.Equal(t, tst.expected, result)
+	tests := []struct {
+		input    string
+		expected string
+	}{
+		{input: "a4bc2d5e", expected: "aaaabccddddde"},
+		{input: "abccd", expected: "abccd"},
+		{input: "", expected: ""},
+		{input: "aaa0b", expected: "aab"},
+		// uncomment if task with asterisk completed
+		// {input: `qwe\4\5`, expected: `qwe45`},
+		// {input: `qwe\45`, expected: `qwe44444`},
+		// {input: `qwe\\5`, expected: `qwe\\\\\`},
+		// {input: `qwe\\\3`, expected: `qwe\3`},
+	}
+
+	for _, tc := range tests {
+		tc := tc
+		t.Run(tc.input, func(t *testing.T) {
+			result, err := Unpack(tc.input)
+			require.NoError(t, err)
+			require.Equal(t, tc.expected, result)
+		})
 	}
 }
 
-func TestUnpackWithEscape(t *testing.T) {
-	t.Skip() // NeedRemove if task with asterisk completed
-
-	for _, tst := range [...]test{
-		{
-			input:    `qwe\4\5`,
-			expected: `qwe45`,
-		},
-		{
-			input:    `qwe\45`,
-			expected: `qwe44444`,
-		},
-		{
-			input:    `qwe\\5`,
-			expected: `qwe\\\\\`,
-		},
-		{
-			input:    `qwe\\\3`,
-			expected: `qwe\3`,
-		},
-	} {
-		result, err := Unpack(tst.input)
-		require.Equal(t, tst.err, err)
-		require.Equal(t, tst.expected, result)
+func TestUnpackInvalidString(t *testing.T) {
+	invalidStrings := []string{"3abc", "45", "aaa10b"}
+	for _, tc := range invalidStrings {
+		tc := tc
+		t.Run(tc, func(t *testing.T) {
+			_, err := Unpack(tc)
+			require.Truef(t, errors.Is(err, ErrInvalidString), "actual error %q", err)
+		})
 	}
 }
