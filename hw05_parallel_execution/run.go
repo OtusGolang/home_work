400d301a067d141fd31eb1294b01f29d0f144d4d
Stanislav Kondratiev <technobulka@gmail.com>
2021-01-27 15:07:52 +0300

    hw05: fix for linter "gocritic" (#25)
    * hw05: fix for linter "gocritc"
    
    https://go-critic.github.io/overview.html#captLocal-ref
    
    * Update README.md
    
    * Update run.go

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -8,8 +8,8 @@ var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 
 type Task func() error
 
-// Run starts tasks in N goroutines and stops its work when receiving M errors from tasks
-func Run(tasks []Task, N, M int) error {
+// Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
+func Run(tasks []Task, n, m int) error {
 	// Place your code here
 	return nil
 }
