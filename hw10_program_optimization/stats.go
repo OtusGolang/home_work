b4f5c9c6d8f5bdb4194414bebfbeb6b5941ae602
almevik <almevik@mail.ru>
2021-04-01 11:10:24 +0300

    hw10 completed

--- hw10_program_optimization/stats.go
+++ hw10_program_optimization/stats.go
@@ -1,67 +1,38 @@
 package hw10programoptimization
 
 import (
-	"encoding/json"
-	"fmt"
+	"bufio"
+	"bytes"
 	"io"
-	"io/ioutil"
-	"regexp"
-	"strings"
-)
 
-type User struct {
-	ID       int
-	Name     string
-	Username string
-	Email    string
-	Phone    string
-	Password string
-	Address  string
-}
+	"github.com/valyala/fastjson"
+)
 
 type DomainStat map[string]int
 
 func GetDomainStat(r io.Reader, domain string) (DomainStat, error) {
-	u, err := getUsers(r)
-	if err != nil {
-		return nil, fmt.Errorf("get users error: %w", err)
-	}
-	return countDomains(u, domain)
-}
-
-type users [100_000]User
-
-func getUsers(r io.Reader) (result users, err error) {
-	content, err := ioutil.ReadAll(r)
-	if err != nil {
-		return
-	}
-
-	lines := strings.Split(string(content), "\n")
-	for i, line := range lines {
-		var user User
-		if err = json.Unmarshal([]byte(line), &user); err != nil {
-			return
-		}
-		result[i] = user
-	}
-	return
-}
-
-func countDomains(u users, domain string) (DomainStat, error) {
+	var fParser fastjson.Parser
 	result := make(DomainStat)
+	searchBytes := []byte("." + domain)
+	delimiter := []byte("@")
 
-	for _, user := range u {
-		matched, err := regexp.Match("\\."+domain, []byte(user.Email))
+	bScanner := bufio.NewScanner(r)
+
+	for bScanner.Scan() {
+		val, err := fParser.ParseBytes(bScanner.Bytes())
 		if err != nil {
 			return nil, err
 		}
 
-		if matched {
-			num := result[strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])]
-			num++
-			result[strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])] = num
+		email := val.GetStringBytes("Email")
+		email = bytes.ToLower(email)
+		email = bytes.TrimSpace(email)
+
+		// Если email заканчивается на нужный домен
+		if bytes.HasSuffix(email, searchBytes) {
+			result[string(bytes.SplitN(email, delimiter, 2)[1])]++
 		}
 	}
+
 	return result, nil
 }
