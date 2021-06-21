51fb339fed1100f25d68c42126b9ba6d22df1885
Aleksandr Melkikh <aleksandr.melkih@gs-labs.ru>
2021-06-21 13:46:41 +0200

    hw01_hello_otus: reverse test string

--- hw01_hello_otus/go.mod
+++ hw01_hello_otus/go.mod
@@ -1,3 +1,5 @@
-module github.com/fixme_my_friend/hw01_hello_otus
+module github.com/OtusGolang/hw01_hello_otus
 
 go 1.16
+
+require golang.org/x/example v0.0.0-20210407023211-09c3a5e06b5d
--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+	"golang.org/x/example/stringutil"
+)
+
 func main() {
-	// Place your code here.
+	reversedStr := stringutil.Reverse("Hello, OTUS!")
+	fmt.Println(reversedStr)
 }
