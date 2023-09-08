dda3e40473e4cea7ac67575ca90e681229dd79e2
Nataliya <korovina@ittest-team.ru>
2023-09-08 15:22:06 +0300

    Task: hw01_hello_otus Homework #1

--- hw01_hello_otus/go.mod
+++ hw01_hello_otus/go.mod
@@ -1,3 +1,5 @@
-module github.com/fixme_my_friend/hw01_hello_otus
+module github.com/Nataliya48/home_work/hw01_hello_otus
 
-go 1.19
+go 1.21
+
+require golang.org/x/example v0.0.0-20170904185048-46695d81d1fa
--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"fmt"
+	"golang.org/x/example/stringutil"
+)
+
 func main() {
-	// Place your code here.
+	str := "Hello, OTUS!"
+	str = stringutil.Reverse(str)
+	fmt.Println(str)
 }
