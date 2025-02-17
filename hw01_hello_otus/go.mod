7be02943677fb29330474fe4150f077204dde70c
Kirill <kuznetsokirill@yandex.ru>
2025-02-18 01:40:05 +0300

    HW1 is completed

--- hw01_hello_otus/go.mod
+++ hw01_hello_otus/go.mod
@@ -1,3 +1,5 @@
 module github.com/fixme_my_friend/hw01_hello_otus
 
 go 1.22
+
+require golang.org/x/example/hello v0.0.0-20241216154601-40afcb705d05
--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+
+	"golang.org/x/example/hello/reverse"
+)
+
 func main() {
-	// Place your code here.
+	fmt.Println(reverse.String("Hello, OTUS!"))
 }
